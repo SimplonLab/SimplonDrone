4144a34d37637202097433f53aed24c4f0e4f0bd
mehdimaker <contact@mehdimaker.com>
2016-02-06 15:50:30 +0100

    modif install

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
-sudo apt-get install git
-sudo apt-get install gedit
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
 
+#install git
+sudo apt-get install git
+#install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
+#clone du dossier simplon drone
 cd /home/simplonco/Desktop
 cd /home/simplon/Desktop
 cd /home/simplonco/Bureau
 cd /home/simplon/Bureau
 git clone https://github.com/SimplonLab/SimplonDrone.git
-
+#install des packages
 cd /home/simplonco/Desktop/SimplonDrone
 cd /home/simplon/Desktop/SimplonDrone
 cd /home/simplonco/Bureau/SimplonDrone

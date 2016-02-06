9dfc57a6e03a6e72906f3a92d1ca96ffe1fb843b
mehdimaker <contact@mehdimaker.com>
2016-02-06 15:36:10 +0100

    modif install

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 sudo apt-get install git
 sudo apt-get install gedit
+sudo apt-get install node
+sudo apt-get install nodejs
+sudo apt-get install npm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
 nvm install 5.3
+nvm use 5.3
 
 
 cd /home/simplonco/desktop

4c842cc0f70373be7f072a2221a856988ff70a67
mehdimaker <contact@mehdimaker.com>
2016-02-06 12:12:15 +0100

    add realapp

---
+++ realapp.js
@@ -0,0 +1,63 @@
+var five = require("johnny-five");
+var board = new five.Board();
+var arDrone = require('ar-drone');
+var client  = arDrone.createClient();
+
+board.on("ready", function() {
+  /*
+  telnet 192.168.1.1
+  vi /data/config.ini
+
+  */
+
+  // BUTTON Decollage/Atterrissage
+  var button = new five.Button(3);
+  button.on("down", function() {
+    console.log('Take Off !');
+    client.takeoff();
+  });
+  button.on("up", function() {
+    console.log('Land !');
+    client.land();
+  });
+
+  //JOYSTICK Altitude
+  var joystickAltitude = new five.Joystick({
+  //   [ x, y ]
+    pins: ["A1", "A2"],
+  });
+  joystickAltitude.on("change", function() {
+    // console.log("JoystickAltitude");
+    // console.log("  x : ", this.x);
+    // console.log("  y : ", this.y);
+    // console.log("--------------------------------------");
+    if (this.y>0){
+      client.up(this.y);
+    }else{
+      client.down(Math.abs(this.y));
+    }
+  });
+
+  //JOYSTICK Direction
+  var joystickDirection = new five.Joystick({
+    //   [ x, y ]
+    pins: ["A5", "A6"],
+  });
+  joystickDirection.on("change", function() {
+    // console.log("JoystickDirection");
+    // console.log("  x : ", this.x);
+    // console.log("  y : ", this.y);
+    // console.log("--------------------------------------");
+    if (this.y>0){
+      client.front(this.y);
+    }else{
+      client.back(Math.abs(this.y));
+    }
+    if (this.x>0){
+      client.right(this.x);
+    }else{
+      client.left(Math.abs(this.x));
+    }
+  });
+
+});
